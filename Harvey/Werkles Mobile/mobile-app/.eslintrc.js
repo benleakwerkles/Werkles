ea057dd372d13219130aabdd606e946002313677
benleakwerkles <ben.leak@kindsir.com>
2026-07-16 00:12:34 -0400

    Separate substantive lint from formatting policy

--- Harvey/Werkles Mobile/mobile-app/.eslintrc.js
+++ Harvey/Werkles Mobile/mobile-app/.eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   root: true,
   extends: ['@react-native'],
   rules: {
+    'comma-dangle': 'off',
+    'prettier/prettier': 'off',
     'react/react-in-jsx-scope': 'off'
   }
 };
-
